afd09dbe33f8fa3d0574661d794e4fa4b1fc64bb
Christoph Giesel <mail@cgiesel.de>
2013-08-08 21:39:23 +0200

    docstrings on the right position

--- poebelmiezer.py
+++ poebelmiezer.py
@@ -1,33 +1,35 @@
 from twitter import Twitter, TwitterHTTPError
 
-"""class for twitter follower management"""
 class Poebelmiezer:
-    """
-    :param oauth: twitter.OAuth object for twitter
-    """
+    """class for twitter follower management"""
+
     def __init__(self, oauth):
+        """
+        :param oauth: twitter.OAuth object for twitter
+        """
         self.to_follow = set()
         self.all_follower = []
         self.t = Twitter(auth = oauth)
 
-    """follow all users found with the other functions (follower list)"""
     def follow_all(self):
+        """follow all users found with the other functions (follower list)"""
         for f_id in self.to_follow:
             self.t.friendships.create(user_id=f_id)
         self.to_follow.clear()
 
-    """go through all follower and save the non followed and non protected users to the follower list"""
     def search_new_follower(self):
+        """go through all follower and save the non followed and non protected users to the follower list"""
         self.fetch_all_follower()
 
         for f in self.all_follower:
             if not f['following'] and not f['protected']:
                 self.to_follow.add(f['id'])
 
-    """fetch all follower and save them in a list
-    :param cursor: a cursor for the next page for the api (to begin set to None)
-    """
     def fetch_all_follower(self, cursor=None):
+        """fetch all follower and save them in a list
+
+        :param cursor: a cursor for the next page for the api (to begin set to None)
+        """
         try:
             if cursor:
                 results = self.t.followers.list(skip_status=True, include_user_entities=False, cursor=cursor)
@@ -46,19 +48,21 @@ class Poebelmiezer:
         if results['next_cursor'] != 0:
             self.fetch_all_follower(results['next_cursor'])
 
-    """calls get_ff_suggestions n times
-    :param count: count of fcuntion calls
-    """
     def look_for_followfriday(self, count=1):
+        """calls get_ff_suggestions n times
+
+        :param count: count of fcuntion calls
+        """
         max_id = self.get_ff_suggestions() - 1
         for i in range(count - 1):
             max_id = self.get_ff_suggestions(max_id) - 1
 
-    """fetch the timeline, look for tweets with hashtag #ff and save all suggested users to the follow list
-    :param max_id: maximum id of tweets to fetch (older or equal tweets)
-    :returns: the id of the last fetched tweet
-    """
     def get_ff_suggestions(self, max_id=None):
+        """fetch the timeline, look for tweets with hashtag #ff and save all suggested users to the follow list
+
+        :param max_id: maximum id of tweets to fetch (older or equal tweets)
+        :returns: the id of the last fetched tweet
+        """
         try:
             if max_id:
                 timeline = self.t.statuses.home_timeline(include_entities=True, max_id=max_id)
